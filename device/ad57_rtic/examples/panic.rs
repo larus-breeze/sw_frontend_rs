b749c8182436481a4e403e01822de47400e8b77b
Winfried Simon <winfried.simon@gmail.com>
2023-11-19 12:33:56 +0100

    Add panic and watchdog example

---
+++ device/ad57_rtic/examples/panic.rs
@@ -0,0 +1,114 @@
+#![no_main]
+#![no_std]
+
+mod utils;
+
+use embedded_storage::nor_flash::NorFlash;
+use defmt::trace;
+use defmt_rtt as _;
+
+
+use cortex_m_rt::entry;
+use stm32f4xx_hal::{
+    pac::{CorePeripherals, Peripherals},
+    watchdog::IndependentWatchdog,
+    prelude::*, flash::{LockedFlash, FlashExt},
+};
+
+use utils::*;
+
+pub fn delay_ms(millis: u32) {
+    let cycles = millis * 168_000;
+    cortex_m::asm::delay(cycles)
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Error {
+    EepromOrI2c1,
+    NoItemAvailable,
+}
+
+const FLASH_START: u32 = 0x0800_0000;
+const PANIC_BUF: u32 = 0x0807_c000;
+const ERR_MSG_LEN: usize = 128;
+const PANIC_BUF_END: u32 = 0x0808_0000 - ERR_MSG_LEN as u32;
+
+fn write_to_flash<'a>(msg: &'a str) {
+    let mut ptr = PANIC_BUF;
+    while ptr < PANIC_BUF_END {
+        let b = unsafe { *(ptr as *const u8) };
+        if b == 0xff {
+            break
+        }
+        ptr += 1;
+    }
+    let dp = unsafe { stm32f4xx_hal::pac::Peripherals::steal() };
+    let mut flash = LockedFlash::new(dp.FLASH);
+    let mut uflash = flash.unlocked();
+    let _ = NorFlash::write(&mut uflash, ptr - FLASH_START, msg.as_bytes());
+    drop(uflash);
+}
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    cortex_m::interrupt::disable(); // Please not interrupts any more, we will reset device anyway
+    
+    let msg: Concat<ERR_MSG_LEN> =if let Some(location) = info.location() {
+        Concat::from_str("panic in '")
+            .push_str(location.file())
+            .push_str("' line ")
+            .push_u32(location.line())
+            .push_str("\n")
+    } else {
+        Concat::from_str("panic without location info\n")
+    };
+    trace!("{}", msg.as_str());
+    write_to_flash(msg.as_str());
+loop {}
+}
+
+#[entry]
+fn main() -> ! {
+    // Setup clocks
+    let _cp = CorePeripherals::take().unwrap();
+    let dp = Peripherals::take().unwrap();
+    let rcc = dp.RCC.constrain();
+
+    let _clocks = rcc.cfgr
+        .use_hse(16.MHz())
+        .sysclk(168.MHz())
+        .hclk(168.MHz())
+        .pclk1(42.MHz())
+        .pclk2(84.MHz())
+        .freeze();
+
+    let gpioa = dp.GPIOA.split();
+    let gpiob = dp.GPIOB.split();
+    let gpioc = dp.GPIOC.split();
+
+    let keyboard_pins =
+        KeyboardPins::new(gpioa.pa7, gpioc.pc5, gpiob.pb0, gpiob.pb1, gpioa.pa6);
+    let mut keyboard = Keyboard::new(keyboard_pins);
+
+    let mut watchdog = IndependentWatchdog::new(dp.IWDG);
+    watchdog.start(1000.millis());
+
+    trace!("reset");
+
+    loop {
+        delay_ms(20);
+        watchdog.feed();
+        let key = keyboard.tick();
+        if key != Key::None {
+            trace!("{}", key as u32);
+        }
+        match key {
+            Key::Button4 => loop {},    // Watchdog test
+            Key::Button3 => panic!(),   // Panic test
+            _ => (),
+        }
+    }
+}
+
---
+++ device/ad57_rtic/examples/utils/keyboard.rs
@@ -0,0 +1,121 @@
+use stm32f4xx_hal::gpio::Pin;
+
+const BTN_1: u8 = 0b0000_0001;
+const BTN_2: u8 = 0b0000_0010;
+const BTN_3: u8 = 0b0000_0100;
+const BTN_ESC: u8 = 0b0000_1000;
+const BTN_ENC: u8 = 0b0001_0000;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Key {
+    Button1,
+    Button2,
+    Button3,
+    Button4,
+    ButtonKnob,
+    None,
+}
+
+
+pub struct Keyboard {
+    btn_1: Pin<'A', 7>,
+    btn_2: Pin<'C', 5>,
+    btn_3: Pin<'B', 0>,
+    btn_esc: Pin<'B', 1>,
+    btn_enc: Pin<'A', 6>,
+
+    last_btn_state: u8,
+    first_go_to_0: bool,
+}
+
+pub struct KeyboardPins {
+    pub p1: Pin<'A', 7>,
+    pub p2: Pin<'C', 5>,
+    pub p3: Pin<'B', 0>,
+    pub p4: Pin<'B', 1>,
+    pub p5: Pin<'A', 6>,
+}
+
+impl KeyboardPins {
+    pub fn new(
+        p1: Pin<'A', 7>,
+        p2: Pin<'C', 5>,
+        p3: Pin<'B', 0>,
+        p4: Pin<'B', 1>,
+        p5: Pin<'A', 6>,
+    ) -> Self {
+        KeyboardPins { p1, p2, p3, p4, p5 }
+    }
+}
+
+
+impl Keyboard {
+    /// Create Keys instance and initialize hardware
+    pub fn new(
+        keyboard_pins: KeyboardPins,
+    ) -> Self {
+        // Config the key buttons
+        let btn_1 = keyboard_pins.p1.into_pull_up_input();
+        let btn_2 = keyboard_pins.p2.into_pull_up_input();
+        let btn_3 = keyboard_pins.p3.into_pull_up_input();
+        let btn_esc = keyboard_pins.p4.into_pull_up_input();
+        let btn_enc = keyboard_pins.p5.into_pull_up_input();
+
+Self {
+            btn_1, // 5 port pins for buttons
+            btn_2,
+            btn_3,
+            btn_esc,
+            btn_enc,
+
+            last_btn_state: 0,    // last state of buttons
+            first_go_to_0: false, // we have to wait, befor we accept new key events
+        }
+    }
+
+    /// Regular polling of the front keys and the rotary encoders
+    ///
+    /// This routine is called about every 20 milliseconds and queries the 5 keys and the two
+    /// rotary encoders. The result is pushed into the key event queue.
+    pub fn tick(&mut self) -> Key {
+        // First of all, the pushbuttons are scanned. The events are summarized in a status
+        // variable in order to evaluate them afterwards. This allows single buttons to be
+        // used in the same way as button patterns.
+        let mut btn_state = 0u8;
+
+        if self.btn_1.is_low() {
+            btn_state |= BTN_1
+        }
+        if self.btn_2.is_low() {
+            btn_state |= BTN_2
+        }
+        if self.btn_3.is_low() {
+            btn_state |= BTN_3
+        }
+        if self.btn_esc.is_low() {
+            btn_state |= BTN_ESC
+        }
+        if self.btn_enc.is_low() {
+            btn_state |= BTN_ENC
+        }
+
+        if self.first_go_to_0 {
+            if btn_state == 0 {
+                self.first_go_to_0 = false;
+            }
+        } else if btn_state < self.last_btn_state {
+            // Triggers when first key is released
+            self.first_go_to_0 = true;
+            match self.last_btn_state {
+                BTN_1 => return Key::Button1,
+                BTN_2 => return Key::Button2,
+                BTN_3 => return Key::Button3,
+                BTN_ESC => return Key::Button4,
+                BTN_ENC => return Key::ButtonKnob,
+                _ => (),
+            };
+        } 
+        self.last_btn_state = btn_state;
+        Key::None
+    }
+}
