634fd1d1828cac7f66d3b4f9e6ae14d31a4bd9bd
Winfried Simon <winfried.simon@gmail.com>
2023-11-28 12:13:41 +0100

    Simplify r61580 display driver

--- device/ad57_rtic/src/driver/r61580.rs
+++ device/ad57_rtic/src/driver/r61580.rs
@@ -1,10 +1,9 @@
-// associated re-typing not supported in rust yet
-#![allow(clippy::type_complexity)]
-
 //! This crate provides a ST7789 driver to connect to TFT displays.
 
+use display_interface::{DataFormat::{U16BEIter, U8Iter}, WriteOnlyDataCommand};
 use embedded_hal::digital::v2::OutputPin;
 
+use core::iter::once;
 use crate::driver::init::delay_ms;
 use crate::utils::DevError;
 
@@ -14,7 +13,7 @@ pub const PORTRAIT_TOTAL_HEIGHT: u16 = 320;
 pub const LANDSCAPE_TOTAL_WIDTH: u16 = PORTRAIT_TOTAL_HEIGHT;
 #[allow(dead_code)]
 pub const LANDSCAPE_TOTAL_HEIGHT: u16 = PORTRAIT_TOTAL_WIDTH;
-pub const TOTAL_PIXELS: u32 = PORTRAIT_TOTAL_HEIGHT as u32 * PORTRAIT_TOTAL_WIDTH as u32;
+//pub const TOTAL_PIXELS: u32 = PORTRAIT_TOTAL_HEIGHT as u32 * PORTRAIT_TOTAL_WIDTH as u32;
 
 // Visible Window Portrait
 pub const PORTRAIT_ORIGIN_X: u16 = 6;
@@ -49,15 +48,9 @@ pub enum Instruction {
 }
 
 ///
-/// R61580 driver to connect to TFT displays.
+/// R61580 driver to connect to LCD displays.
 ///
-pub struct R61580<RST>
-where
-    RST: OutputPin,
-{
-    // Reset pin.
-    rst: RST,
-}
+pub struct R61580 {}
 
 ///
 /// Display orientation.
@@ -77,9 +70,7 @@ impl Default for Orientation {
 }
 
 #[allow(unused)]
-impl<RST, PinE> R61580<RST>
-where
-    RST: OutputPin<Error = PinE>,
+impl R61580
 {
     ///
     /// Creates a new ST7789 driver instance
@@ -91,124 +82,119 @@ where
     /// * `size_x` - x axis resolution of the display in pixels
     /// * `size_y` - y axis resolution of the display in pixels
     ///
-    pub fn new(rst: RST) -> Self {
-        R61580 { rst }
-    }
+    pub fn new<DI, RST, PinE>(di: &mut DI, mut rst: RST) -> Self 
+    where
+        RST: OutputPin<Error = PinE>,
+        DI: WriteOnlyDataCommand,
+    {
 
-    pub fn init(&mut self) {
-        let _ = self.hard_reset();
+        // Performs a hard reset using the RST pin sequence
+        let _ = rst.set_high();
+        delay_ms(1); // ensure the pin change will get registered
+        let _ = rst.set_low();
+        delay_ms(2); // ensure the pin change will get registered
+        let _ = rst.set_high();
+        delay_ms(2); // ensure the pin change will get registered
+
 
         // Check, if display answers
-        self.write_command(0);
-        let id = self.read_data();
+        write_command(di, 0);
+        let id = read_data();
 
         // 4x RS=0
-        self.write_command_and_data(0x0000, 0x0000);
-        self.write_command_and_data(0x0000, 0x0000);
-        self.write_command_and_data(0x0000, 0x0000);
-        self.write_command_and_data(0x0000, 0x0000);
+        write_command_and_data(di, 0x0000, 0x0000);
+        write_command_and_data(di, 0x0000, 0x0000);
+        write_command_and_data(di, 0x0000, 0x0000);
+        write_command_and_data(di, 0x0000, 0x0000);
         delay_ms(200);
         // Setup display
-        self.write_command_and_data(0x00A4, 0x0001); // NVW Calibration: CALB=1
+        write_command_and_data(di, 0x00A4, 0x0001); // NVW Calibration: CALB=1
         delay_ms(1);
 
-        self.write_command_and_data(0x0060, 0xA700); // Driver Output Control 2: GS=1, NL=0x27, SCN=0
-        self.write_command_and_data(0x0008, 0x0503); // Display Control 2: BP=3, FP=5
+        write_command_and_data(di, 0x0060, 0xA700); // Driver Output Control 2: GS=1, NL=0x27, SCN=0
+        write_command_and_data(di, 0x0008, 0x0503); // Display Control 2: BP=3, FP=5
 
-        self.write_command_and_data(0x0030, 0x0500); // y control
-        self.write_command_and_data(0x0031, 0x3711); // y control
-        self.write_command_and_data(0x0032, 0x0605); // y control
-        self.write_command_and_data(0x0033, 0x120D); // y control
-        self.write_command_and_data(0x0034, 0x1202); // y control
-        self.write_command_and_data(0x0035, 0x0D0A); // y control
-        self.write_command_and_data(0x0036, 0x3506); // y control
-        self.write_command_and_data(0x0037, 0x1107); // y control
-        self.write_command_and_data(0x0038, 0x0005); // y control
-        self.write_command_and_data(0x0039, 0x0212); // y control
+        write_command_and_data(di, 0x0030, 0x0500); // y control
+        write_command_and_data(di, 0x0031, 0x3711); // y control
+        write_command_and_data(di, 0x0032, 0x0605); // y control
+        write_command_and_data(di, 0x0033, 0x120D); // y control
+        write_command_and_data(di, 0x0034, 0x1202); // y control
+        write_command_and_data(di, 0x0035, 0x0D0A); // y control
+        write_command_and_data(di, 0x0036, 0x3506); // y control
+        write_command_and_data(di, 0x0037, 0x1107); // y control
+        write_command_and_data(di, 0x0038, 0x0005); // y control
+        write_command_and_data(di, 0x0039, 0x0212); // y control
 
-        self.write_command_and_data(0x0090, 0x001D); // Panel I/F Control 1: DIVI=0, RTNI=0x1D (80Hz??)
+        write_command_and_data(di, 0x0090, 0x001D); // Panel I/F Control 1: DIVI=0, RTNI=0x1D (80Hz??)
 
-        self.write_command_and_data(0x009C, 0x0043);
+        write_command_and_data(di, 0x009C, 0x0043);
 
-        self.write_command_and_data(0x0010, 0x0310); // Power Control 1: BT=2, AP=1, DSTB=0
-        self.write_command_and_data(0x0011, 0x0231); // Power Control 2: DC1=2, DC0=3, VC=1
-        self.write_command_and_data(0x0012, 0x01BC); // Power Control 3: VRH=0, VCMR=1, PSON=0, PON=0, VRH=0x0C
-        self.write_command_and_data(0x0013, 0x1400); // Power Control 4: VDV=0x14,
+        write_command_and_data(di, 0x0010, 0x0310); // Power Control 1: BT=2, AP=1, DSTB=0
+        write_command_and_data(di, 0x0011, 0x0231); // Power Control 2: DC1=2, DC0=3, VC=1
+        write_command_and_data(di, 0x0012, 0x01BC); // Power Control 3: VRH=0, VCMR=1, PSON=0, PON=0, VRH=0x0C
+        write_command_and_data(di, 0x0013, 0x1400); // Power Control 4: VDV=0x14,
 
         delay_ms(100);
 
-        self.write_command_and_data(0x0001, 0x0500); // Driver Output Control 1: SM=1, SS=1
-        self.write_command_and_data(0x0002, 0x0200); // LCD Driving Control: BC0=1, NW0=0
-        self.write_command_and_data(0x0003, 0x1030); // Entry Mode: TRIREG=0, DFM=0, BGR=1, ORG=0, I/D=3, AM=0
+        write_command_and_data(di, 0x0001, 0x0500); // Driver Output Control 1: SM=1, SS=1
+        write_command_and_data(di, 0x0002, 0x0200); // LCD Driving Control: BC0=1, NW0=0
+        write_command_and_data(di, 0x0003, 0x1030); // Entry Mode: TRIREG=0, DFM=0, BGR=1, ORG=0, I/D=3, AM=0
 
         delay_ms(1);
 
-        self.write_command_and_data(0x000A, 0x0008); // Display Control 4: FMARK0=1, FM=0
+        write_command_and_data(di, 0x000A, 0x0008); // Display Control 4: FMARK0=1, FM=0
 
-        self.write_command_and_data(0x0091, 0x0003); // Panel I/F Control 1-1: SPCWI=3
-        self.write_command_and_data(0x0093, 0x0201); // Panel I/F Control 3: VEQWI=2, MCPI=1
+        write_command_and_data(di, 0x0091, 0x0003); // Panel I/F Control 1-1: SPCWI=3
+        write_command_and_data(di, 0x0093, 0x0201); // Panel I/F Control 3: VEQWI=2, MCPI=1
 
-        self.write_command_and_data(0x0007, 0x0100); // Display Control 1: BASEE=1
+        write_command_and_data(di, 0x0007, 0x0100); // Display Control 1: BASEE=1
         delay_ms(35);
-
-        // blank entire HW RAM contents
-        let colors = core::iter::repeat(0).take(TOTAL_PIXELS as usize);
-        self.set_pixels(0, 0, colors);
-    }
-
-    ///
-    /// Performs a hard reset using the RST pin sequence
-    ///
-    pub fn hard_reset(&mut self) -> Result<(), DevError> {
-        self.rst.set_high().map_err(|_| DevError::PinError)?;
-        delay_ms(1); // ensure the pin change will get registered
-        self.rst.set_low().map_err(|_| DevError::PinError)?;
-        delay_ms(2); // ensure the pin change will get registered
-        self.rst.set_high().map_err(|_| DevError::PinError)?;
-        delay_ms(2); // ensure the pin change will get registered
-        Ok(())
+        R61580 { }
     }
 
     ///
     /// Sets display orientation
     ///
     #[allow(unused)]
-    pub fn set_orientation(&mut self, orientation: Orientation) -> Result<(), DevError> {
+    pub fn set_orientation<DI>(&mut self, di: &mut DI, orientation: Orientation) -> Result<(), DevError> 
+    where
+        DI: WriteOnlyDataCommand,
+    {
         match orientation {
             Orientation::Portrait => {
-                //self.write_command_and_data(0x03, 0x1030);
+                //write_command_and_data(di, 0x03, 0x1030);
                 delay_ms(30);
-                self.write_command_and_data(Instruction::PosX as u8, 0);
-                self.write_command_and_data(Instruction::PosY as u8, 0);
-                self.write_command_and_data(Instruction::HSA as u8, PORTRAIT_ORIGIN_X);
-                self.write_command_and_data(
+                write_command_and_data(di, Instruction::PosX as u8, 0);
+                write_command_and_data(di, Instruction::PosY as u8, 0);
+                write_command_and_data(di, Instruction::HSA as u8, PORTRAIT_ORIGIN_X);
+                write_command_and_data(di, 
                     Instruction::HEA as u8,
                     PORTRAIT_ORIGIN_X + PORTRAIT_AVAIL_WIDTH - 1,
                 );
-                self.write_command_and_data(
+                write_command_and_data(di, 
                     Instruction::VSA as u8,
                     PORTRAIT_TOTAL_HEIGHT - PORTRAIT_AVAIL_HEIGHT - PORTRAIT_ORIGIN_Y + 1,
                 );
-                self.write_command_and_data(
+                write_command_and_data(di, 
                     Instruction::VEA as u8,
                     PORTRAIT_TOTAL_HEIGHT - PORTRAIT_ORIGIN_Y,
                 );
-                self.write_command_and_data(0x03, 0x1030);
+                write_command_and_data(di, 0x03, 0x1030);
             }
             Orientation::Landscape => {
-                self.write_command_and_data(0x03, 0x1098);
-                self.write_command_and_data(Instruction::PosX as u8, 0);
-                self.write_command_and_data(Instruction::PosY as u8, 0);
-                self.write_command_and_data(Instruction::HSA as u8, LANDSCAPE_ORIGIN_Y);
-                self.write_command_and_data(
+                write_command_and_data(di, 0x03, 0x1098);
+                write_command_and_data(di, Instruction::PosX as u8, 0);
+                write_command_and_data(di, Instruction::PosY as u8, 0);
+                write_command_and_data(di, Instruction::HSA as u8, LANDSCAPE_ORIGIN_Y);
+                write_command_and_data(di, 
                     Instruction::HEA as u8,
                     LANDSCAPE_ORIGIN_Y + LANDSCAPE_AVAIL_HEIGHT - 1,
                 );
-                self.write_command_and_data(
+                write_command_and_data(di, 
                     Instruction::VSA as u8,
                     LANDSCAPE_TOTAL_WIDTH - LANDSCAPE_AVAIL_WIDTH - LANDSCAPE_ORIGIN_X + 1,
                 );
-                self.write_command_and_data(
+                write_command_and_data(di, 
                     Instruction::VEA as u8,
                     LANDSCAPE_TOTAL_WIDTH - LANDSCAPE_ORIGIN_X,
                 );
@@ -216,67 +202,26 @@ where
         }
         Ok(())
     }
+}
 
-    ///
-    /// Sets a pixel color at the given coords.
-    ///
-    /// # Arguments
-    ///
-    /// * `x` - x coordinate
-    /// * `y` - y coordinate
-    /// * `color` - the Rgb565 color value
-    ///
-    #[allow(unused)]
-    pub fn set_pixel(&mut self, x: u16, y: u16, color: u16) {
-        self.write_command_and_data(Instruction::PosX as u8, x);
-        self.write_command_and_data(Instruction::PosY as u8, y);
-        self.write_command_and_data(Instruction::Gram as u8, color);
-    }
+#[inline]
+fn write_command<DI: WriteOnlyDataCommand>(di: &mut DI, cmd: u8) {
+    let _ = di.send_commands(U8Iter(&mut once(cmd)));
+}
 
-    ///
-    /// Sets pixel colors in given rectangle bounds.
-    ///
-    /// # Arguments
-    ///
-    /// * `x` - x coordinate
-    /// * `y` - y coordinate
-    /// * `colors` - anything that can provide `IntoIterator<Item = u16>` to iterate over pixel data
-    ///
-    pub fn set_pixels<T>(&mut self, x: u16, y: u16, colors: T)
-    where
-        T: IntoIterator<Item = u16>,
-    {
-        self.write_command_and_data(Instruction::PosX as u8, x);
-        self.write_command_and_data(Instruction::PosY as u8, y);
-        self.write_command(Instruction::Gram as u8);
-        for pixel in colors {
-            self.write_data(pixel);
-        }
-    }
+#[inline]
+fn write_data<DI: WriteOnlyDataCommand>(di: &mut DI, data: u16) {
+    let _ = di.send_data(U16BEIter(&mut once(data)));
+}
 
-    const LCD_CMD_ADDRESS: usize = 0x60000000;
-    const LCD_DATA_ADDRESS: usize = 0x60020000;
+#[inline]
+fn read_data() -> u16 {
+    // Safety: Reading u16 is atomic, so unsafe is ok
+    unsafe { core::ptr::read_volatile(0x6002_0000 as *const u16) }
+}
 
-    #[inline]
-    pub fn write_command(&mut self, cmd: u8) {
-        // Safety: Writing u8 is atomic, so unsafe is ok
-        unsafe { core::ptr::write_volatile(0x6000_0000 as *mut u8, cmd) }
-    }
-
-    #[inline]
-    pub fn write_data(&mut self, data: u16) {
-        // Safety: Writing u16 is atomic, so unsafe is ok
-        unsafe { core::ptr::write_volatile(0x6002_0000 as *mut u16, data) };
-    }
-
-    #[inline]
-    pub fn read_data(&self) -> u16 {
-        // Safety: Reading u16 is atomic, so unsafe is ok
-        unsafe { core::ptr::read_volatile(0x6002_0000 as *const u16) }
-    }
-
-    pub fn write_command_and_data(&mut self, cmd: u8, data: u16) {
-        self.write_command(cmd);
-        self.write_data(data)
-    }
+#[inline]
+fn write_command_and_data<DI: WriteOnlyDataCommand>(di: &mut DI, cmd: u8, data: u16) {
+    write_command(di, cmd);
+    write_data(di, data)
 }
