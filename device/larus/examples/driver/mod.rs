5dd6a45c69647ce3f2d0568f8a9a7bb0667b9341
Winfried Simon <winfried.simon@gmail.com>
2023-12-14 17:39:31 +0100

    Add example for keyboard driver

--- device/larus/examples/driver/mod.rs
+++ device/larus/examples/driver/mod.rs
@@ -1,4 +1,14 @@
+#[allow(unused)]
+use corelib::Event;
+#[allow(unused)]
+use heapless::mpmc::MpMcQueue;
+
+#[allow(unused)]
 mod fmc_lcd;
+#[allow(unused)]
+mod keyboard;
 
 pub use fmc_lcd::*;
+pub use keyboard::*;
 
+pub type QEvents = MpMcQueue<Event, 8>;
---
+++ device/larus/examples/keys.rs
@@ -0,0 +1,67 @@
+#![no_main]
+#![no_std]
+mod driver;
+
+use defmt::*;
+use defmt_rtt as _;
+use panic_rtt_target as _;
+
+use corelib::Event::KeyItem;
+use corelib::*;
+use cortex_m_rt::entry;
+use heapless::mpmc::MpMcQueue;
+use stm32h7xx_hal::{pac, prelude::*};
+
+use driver::*;
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    info!("init");
+
+    // Constrain and Freeze power
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
+
+    // Constrain and Freeze clock
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
+
+    let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
+
+    // Get the delay provider.
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    // This queue routes the events to the controller.
+    static Q_EVENTS: QEvents = MpMcQueue::new();
+
+    let mut keyboard = {
+        let keyboard_pins = KeyboardPins::new(gpioe.pe5, gpioe.pe6, gpioe.pe4);
+        let enc1_res = Enc1Res::new(ccdr.peripheral.TIM5, dp.TIM5, gpioa.pa0, gpioa.pa1);
+        let enc2_res = Enc2Res::new(ccdr.peripheral.TIM3, dp.TIM3, gpiob.pb4.into(), gpioa.pa7);
+        Keyboard::new(keyboard_pins, enc1_res, enc2_res, &Q_EVENTS)
+    };
+
+    loop {
+        delay.delay_ms(20_u16);
+        keyboard.tick();
+        while let Some(event) = Q_EVENTS.dequeue() {
+            if let KeyItem(key_event) = event {
+                match key_event {
+                    KeyEvent::Btn1 => trace!("Btn1"),
+                    KeyEvent::Btn2 => trace!("Btn2"),
+                    KeyEvent::BtnEnc => trace!("BtnEnc"),
+                    KeyEvent::Rotary1Left => trace!("Rotary1Left"),
+                    KeyEvent::Rotary1Right => trace!("Rotary1Right"),
+                    KeyEvent::Rotary2Left => trace!("Rotary2Left"),
+                    KeyEvent::Rotary2Right => trace!("Rotary2Right"),
+                    _ => (),
+                }
+            }
+        }
+    }
+}
