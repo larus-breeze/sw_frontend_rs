181d49d813e49aa502e86af4a70d5e11967ea2ae
Winfried Simon <winfried.simon@gmail.com>
2023-12-15 15:51:24 +0100

    CAN loopback without interrupts

---
+++ device/larus/examples/can.rs
@@ -0,0 +1,161 @@
+#![no_main]
+#![no_std]
+
+mod driver;
+
+use nb::block;
+use core::num::{NonZeroU16, NonZeroU8};
+//use core::{cell::RefCell, num::{NonZeroU16, NonZeroU8}};
+use defmt::*;
+use defmt_rtt as _;
+use panic_rtt_target as _;
+//use cortex_m::{asm, interrupt::Mutex};
+use cortex_m_rt::entry;
+use stm32h7xx_hal::{
+    pac::{
+        CorePeripherals, Peripherals as DevicePeripherals, interrupt, NVIC, //FDCAN1
+    },
+    prelude::*,
+//    can,
+    gpio::Speed,
+    rcc::{PllConfigStrategy, rec}, //device::fdcan1,
+};
+use fdcan::{
+//    FdCan, ConfigMode, InternalLoopbackMode,
+    interrupt::Interrupt,
+    config::NominalBitTiming,
+    filter::{StandardFilter, StandardFilterSlot},
+    frame::{FrameFormat, TxFrameHeader},
+    id::StandardId,
+};
+
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Error {
+    EepromOrI2c1,
+    NoItemAvailable,
+}
+
+#[entry]
+fn main() -> ! {
+    // Setup clocks
+    let mut cp = CorePeripherals::take().unwrap();
+    let dp = DevicePeripherals::take().unwrap();
+
+    info!("init");
+
+    // Constrain and freeze power
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
+
+    // Initialize clock system
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc
+        .use_hse(25.MHz())
+        .sys_ck(192.MHz())
+        .hclk(192.MHz()) // FMC clock from HCLK by default
+        .pll1_strategy(PllConfigStrategy::Iterative)
+        .pll1_q_ck(32.MHz())
+        .pll2_p_ck(96.MHz())
+        .pll2_r_ck(96.MHz())
+        .freeze(pwrcfg, &dp.SYSCFG);
+
+    // Initialize system...
+    cp.SCB.enable_icache();
+    cp.DWT.enable_cycle_counter();
+
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    let fdcan_prec = ccdr
+        .peripheral
+        .FDCAN
+        .kernel_clk_mux(rec::FdcanClkSel::Pll1Q);
+
+    let mut can = {
+        info!("Init CAN 1");
+        let gpioh = dp.GPIOH.split(ccdr.peripheral.GPIOH);
+        let rx = gpioh.ph14.into_alternate().speed(Speed::VeryHigh);
+        let tx = gpioh.ph13.into_alternate().speed(Speed::VeryHigh);
+
+        info!("-- Create CAN 1 instance");
+        dp.FDCAN1.fdcan(tx, rx, fdcan_prec)
+    };
+    
+    can.set_protocol_exception_handling(false);
+    can.enable_interrupt(Interrupt::TxEmpty);
+
+    info!("-- Configure nominal timing");
+    // Kernel Clock 32MHz, Bit rate: 1MBit/s, Sample Point 87.5%
+    // Value was calculated with http://www.bittiming.can-wiki.info/
+    // TODO: use the can_bit_timings crate
+    let data_bit_timing = NominalBitTiming {
+        prescaler: NonZeroU16::new(2).unwrap(),
+        seg1: NonZeroU8::new(13).unwrap(),
+        seg2: NonZeroU8::new(2).unwrap(),
+        sync_jump_width: NonZeroU8::new(1).unwrap(),
+    };
+    can.set_nominal_bit_timing(data_bit_timing);
+
+    info!("-- Configure Filters");
+    can.set_standard_filter(
+        StandardFilterSlot::_0,
+        StandardFilter::accept_all_into_fifo0(),
+    );
+
+    info!("-- Set CAN into normal mode");
+    let mut can = can.into_internal_loopback();
+    //let mut can = can.into_normal();
+
+    /*cortex_m::interrupt::free(|cs| {
+        FDCAN1.borrow(cs).replace(Some(can));
+    });*/
+
+    unsafe {
+        cp.NVIC.set_priority(interrupt::FDCAN1_IT0, 1);
+        NVIC::unmask(interrupt::FDCAN1_IT0);
+    }
+
+
+    info!("Create Message Data");
+    let mut buffer = [
+        0xAA, 0xAA, 0xAA, 0xAA, 0xFF, 0xFF, 0xFF, 0xFF, 0x0, 0x0, 0x0, 0x0,
+        0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+    ];
+    info!("Create Message Header");
+    let header = TxFrameHeader {
+        len: 2 * 4,
+        id: StandardId::new(0x1).unwrap().into(),
+        frame_format: FrameFormat::Standard,
+        bit_rate_switching: false,
+        marker: None,
+    };
+//    info!("Initial Header: {:?}", &header);
+
+    info!("Transmit initial message");
+    block!(can.transmit(header, &buffer)).unwrap();
+
+    loop {
+        if let Ok(rxheader) = block!(can.receive0(&mut buffer)) {
+            //info!("Received Header Id: {:?}", rxheader);
+            info!("received data: {:?}", &buffer);
+
+            delay.delay_ms(1_u16);
+            block!(can.transmit(rxheader.unwrap().to_tx_header(None), &buffer))
+                .unwrap();
+            info!("Transmit: {:?}", buffer);
+        }
+    }
+
+}
+
+/*static FDCAN1: Mutex<RefCell<Option<fdcan::FdCan<can::Can<FDCAN1>, InternalLoopbackMode>>>> =
+    Mutex::new(RefCell::new(None));
+
+#[interrupt]
+fn FDCAN1_IT0() {
+    cortex_m::interrupt::free(|cs| {
+        let mut rc = FDCAN1.borrow(cs).borrow_mut();
+        let can = rc.as_mut().unwrap();
+        can.clear_interrupt(Interrupt::TxEmpty);
+    })
+}*/
---
+++ device/larus/examples/driver/canbus.rs
@@ -0,0 +1,150 @@
+use bxcan::{filter::ListEntry16, Fifo, Frame, Interrupt, StandardId};
+use corelib::CTxFrames;
+use defmt::*;
+use heapless::spsc::{Consumer, Producer, Queue};
+use stm32f4xx_hal::{
+    can::{Can, CanExt},
+    gpio::Pin,
+    pac::CAN1,
+};
+
+use corelib::{frontend, sensor};
+
+// This queue transports the can bus frames from the can rx driver to the controller.
+const MAX_RX_FRAMES: usize = 20;
+pub type QRxFrames = Queue<Frame, MAX_RX_FRAMES>;
+pub type PRxFrames = Producer<'static, Frame, MAX_RX_FRAMES>;
+pub type CRxFrames = Consumer<'static, Frame, MAX_RX_FRAMES>;
+
+/// Initialize peripheral bxcan and generate instances to send and receive can bus frames
+pub fn init_can(
+    can_1: CAN1,
+    tx: Pin<'A', 12>,
+    rx: Pin<'A', 11>,
+    c_tx_frames: CTxFrames,
+    p_rx_frames: PRxFrames,
+) -> (CanTx, CanRx) {
+    let mut can = {
+        let rx = rx.into_alternate::<9>();
+        let tx = tx.into_alternate::<9>();
+        let can = can_1.can((tx, rx));
+        bxcan::Can::builder(can)
+            // APB1 (PCLK1): 42MHz, Bit rate: 1 MBit/s, Sample Point 87.5%
+            // Value was calculated with http://www.bittiming.can-wiki.info/
+            .set_bit_timing(0x001a0002)
+            .enable()
+    };
+
+    let mut filters = can.modify_filters();
+    filters
+        .clear()
+        //.enable_bank(0, Fifo::Fifo0, Mask32::accept_all());
+        .enable_bank(
+            0,
+            Fifo::Fifo0,
+            [
+                ListEntry16::data_frames_with_id(StandardId::new(sensor::AIRSPEED).unwrap()),
+                ListEntry16::data_frames_with_id(StandardId::new(sensor::VARIO).unwrap()),
+                ListEntry16::data_frames_with_id(StandardId::new(sensor::WIND).unwrap()),
+                ListEntry16::data_frames_with_id(StandardId::new(sensor::ATHMOSPHERE).unwrap()),
+            ],
+        )
+        .enable_bank(
+            1,
+            Fifo::Fifo0,
+            [
+                ListEntry16::data_frames_with_id(StandardId::new(sensor::TURN_COORD).unwrap()),
+                ListEntry16::data_frames_with_id(StandardId::new(sensor::ACCELERATION).unwrap()),
+                ListEntry16::data_frames_with_id(StandardId::new(frontend::NOTHING).unwrap()),
+                ListEntry16::data_frames_with_id(StandardId::new(frontend::NOTHING).unwrap()),
+            ],
+        );
+    drop(filters); // Drop filters to leave filter configuraiton mode.
+
+    can.enable_interrupt(Interrupt::Fifo0MessagePending);
+    can.enable_interrupt(Interrupt::TransmitMailboxEmpty);
+
+    let (tx, rx0, _rx1) = can.split();
+    let tx_can = CanTx::new(c_tx_frames, tx);
+    let rx_can = CanRx::new(p_rx_frames, rx0);
+    (tx_can, rx_can)
+}
+
+/// Interrupt service for sending can bus frames
+pub struct CanTx {
+    tx: bxcan::Tx<Can<CAN1>>,
+    c_tx_frames: CTxFrames,
+    extra_frame: Option<Frame>,
+}
+
+impl CanTx {
+    /// Generate the service
+    fn new(c_tx_frames: CTxFrames, tx: bxcan::Tx<Can<CAN1>>) -> Self {
+        CanTx {
+            c_tx_frames,
+            tx,
+            extra_frame: None,
+        }
+    }
+
+    /// Method to call during an active interrupt
+    pub fn on_interrupt(&mut self) {
+        // trace!("Can tx irq");
+
+        self.tx.clear_interrupt_flags(); // we want receive next irqs
+
+        // we first check if there is anything left in the extra frame buffer
+        if let Some(frame) = &self.extra_frame {
+            if let Ok(transmit_status) = self.tx.transmit(frame) {
+                if let Some(frame) = transmit_status.dequeued_frame() {
+                    // Dropping into a mailbox did not work. We need to save the unstored
+                    // frame and wait until something is free again
+                    self.extra_frame = Some(frame.clone());
+                    return; // All mailboxes are full
+                } else {
+                    self.extra_frame = None;
+                    trace!("Extra frame put into can tx mailbox");
+                }
+            }
+        }
+
+        // now we work off the queue
+        while let Some(frame) = self.c_tx_frames.dequeue() {
+            if let Ok(transmit_status) = self.tx.transmit(&frame) {
+                if let Some(frame) = transmit_status.dequeued_frame() {
+                    // Dropping into a mailbox did not work. We need to save the unstored
+                    // frame and wait until something is free again
+                    self.extra_frame = Some(frame.clone());
+                    return; // All mailboxes are full
+                }
+            }
+        }
+    }
+}
+
+/// Interrupt service for receiving can bus frames
+pub struct CanRx {
+    p_rx_frames: PRxFrames,
+    rx0: bxcan::Rx0<Can<CAN1>>,
+}
+
+impl CanRx {
+    /// Create the service
+    fn new(p_rx_frames: PRxFrames, rx0: bxcan::Rx0<Can<CAN1>>) -> Self {
+        CanRx { p_rx_frames, rx0 }
+    }
+
+    /// Call this, when irq is active
+    pub fn on_interrupt(&mut self) {
+        // trace!("Can rx irq");
+        while self.p_rx_frames.capacity() > self.p_rx_frames.len() {
+            match self.rx0.receive() {
+                // silently ignore errors
+                Ok(frame) => {
+                    let _ = self.p_rx_frames.enqueue(frame);
+                }
+                Err(_) => return,
+            }
+        }
+    }
+}
