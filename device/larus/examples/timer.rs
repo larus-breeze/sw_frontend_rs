df0c674118c02049b4316391e2a23376d0690c88
Winfried Simon <winfried.simon@gmail.com>
2023-12-31 13:24:38 +0100

    Add RTIC example

--- device/larus/examples/timer.rs
+++ device/larus/examples/timer.rs
@@ -27,14 +27,10 @@ static TIMER: Mutex<RefCell<Option<MonoTimer>>> = Mutex::new(RefCell::new(None))
 fn main() -> ! {
     let mut cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
-
     info!("init");
 
     let ccdr = set_clocksys!(dp);
-
     let timer = MonoTimer::new(dp.TIM2, ccdr.peripheral.TIM2, &ccdr.clocks);
-    //timer.listen(timer::Event::TimeOut);
-    
 
     cortex_m::interrupt::free(|cs| {
         TIMER.borrow(cs).replace(Some(timer));
