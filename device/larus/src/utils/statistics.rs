3107206697b47e581051e0abc93d6bc39f060a22
Winfried Simon <winfried.simon@gmail.com>
2024-01-29 20:43:32 +0100

    Another change in watchdog pattern

--- device/larus/src/utils/statistics.rs
+++ device/larus/src/utils/statistics.rs
@@ -18,7 +18,7 @@ pub enum Task {
 }
 
 // Pattern defines, which taks must be active to feed the watchdog
-const ALL_ALIVE_PATTERN: u32 = 0b0111_1101;
+const ALL_ALIVE_PATTERN: u32 = 0b1111_1101;
 
 impl Task {
     pub fn from_usize(u: usize) -> Self {
