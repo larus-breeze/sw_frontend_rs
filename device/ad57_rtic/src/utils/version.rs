ac2808653d9b10f0c88c55c4af9e83fc4e4c355d
Winfried Simon <winfried.simon@gmail.com>
2024-02-04 14:56:57 +0100

    Actualize version.rs pack.toml

--- device/ad57_rtic/src/utils/version.rs
+++ device/ad57_rtic/src/utils/version.rs
@@ -3,18 +3,18 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 0, 0],
+    version: [0, 1, 0, 8],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "5b4587";
+pub const GIT_COMIT_HASH: &str = "5efe71";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-01-31T14:55:46+01:0";
+pub const GIT_COMMIT_TIME: &str = "2024-02-04T14:52:07+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.0";
+pub const GIT_TAG_INFO: &str = "v0.1.0-8-g5efe719";
 
 pub const HW_VERSION: HwVersion = HwVersion { 
-    version: [1, 3, 1, 0] 
+    version: [2, 1, 1, 0] 
 };
