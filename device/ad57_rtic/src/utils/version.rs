4ac1152c9be88f48fb32ddb628386c4d7c787112
Winfried Simon <winfried.simon@gmail.com>
2024-02-15 08:33:44 +0100

    Make image for the Air Avionics display

--- device/ad57_rtic/src/utils/version.rs
+++ device/ad57_rtic/src/utils/version.rs
@@ -3,18 +3,18 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 1, 1],
+    version: [0, 1, 2, 1],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "ce794b";
+pub const GIT_COMIT_HASH: &str = "f36cb7";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-02-06T09:01:17+01:0";
+pub const GIT_COMMIT_TIME: &str = "2024-02-15T08:32:38+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.1-1-gce794b2";
+pub const GIT_TAG_INFO: &str = "v0.1.2-1-gf36cb74";
 
 pub const HW_VERSION: HwVersion = HwVersion { 
-    version: [2, 1, 1, 0] 
+    version: [2, 1, 1, 0],
 };
