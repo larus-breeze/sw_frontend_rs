10c5c44390fc8e3a4e1c9ed075161117a365523c
Winfried Simon <winfried.simon@gmail.com>
2024-03-19 10:04:12 +0100

    Make air aivioncs image

--- device/ad57_rtic/src/utils/version.rs
+++ device/ad57_rtic/src/utils/version.rs
@@ -3,18 +3,18 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 2, 1],
+    version: [0, 1, 3, 1],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "f36cb7";
+pub const GIT_COMIT_HASH: &str = "1ea424";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-02-15T08:32:38+01:0";
+pub const GIT_COMMIT_TIME: &str = "2024-03-19T10:03:04+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.2-1-gf36cb74";
+pub const GIT_TAG_INFO: &str = "v0.1.3-1-g1ea4248";
 
-pub const HW_VERSION: HwVersion = HwVersion {
+pub const HW_VERSION: HwVersion = HwVersion { 
     version: [2, 1, 1, 0],
 };
