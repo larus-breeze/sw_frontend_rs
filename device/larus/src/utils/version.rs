de578edfa389bdcada1c38bd3e0731f25b633767
Winfried Simon <winfried.simon@gmail.com>
2024-03-19 10:03:04 +0100

    Make larus image

--- device/larus/src/utils/version.rs
+++ device/larus/src/utils/version.rs
@@ -3,18 +3,18 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 2, 0],
+    version: [0, 1, 3, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "9ac3c6";
+pub const GIT_COMIT_HASH: &str = "60b707";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-02-15T08:25:56+01:0";
+pub const GIT_COMMIT_TIME: &str = "2024-03-19T09:56:52+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.2";
+pub const GIT_TAG_INFO: &str = "v0.1.3";
 
-pub const HW_VERSION: HwVersion = HwVersion {
+pub const HW_VERSION: HwVersion = HwVersion { 
     version: [1, 3, 1, 0],
 };
