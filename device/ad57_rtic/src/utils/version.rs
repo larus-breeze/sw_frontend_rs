c1a27866a8683231863bb72255d3608fbe3b8bd5
Winfried Simon <winfried.simon@gmail.com>
2024-04-19 10:20:47 +0200

    v0.1.4

--- device/ad57_rtic/src/utils/version.rs
+++ device/ad57_rtic/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 3, 1],
+    version: [0, 1, 4, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "1ea424";
+pub const GIT_COMIT_HASH: &str = "8e3751";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-03-19T10:03:04+01:0";
+pub const GIT_COMMIT_TIME: &str = "2024-04-19T10:03:03+02:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.3-1-g1ea4248";
+pub const GIT_TAG_INFO: &str = "v0.1.4";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [2, 1, 1, 0],
--- device/larus/src/utils/version.rs
+++ device/larus/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 3, 0],
+    version: [0, 1, 4, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "60b707";
+pub const GIT_COMIT_HASH: &str = "8e3751";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-03-19T09:56:52+01:0";
+pub const GIT_COMMIT_TIME: &str = "2024-04-19T10:03:03+02:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.3";
+pub const GIT_TAG_INFO: &str = "v0.1.4";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [1, 3, 1, 0],
