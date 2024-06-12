d57d3359a891e9c811936b9f377636cbd8317aa7
Winfried Simon <winfried.simon@gmail.com>
2024-06-12 15:12:26 +0200

    v0.2.0

--- device/larus/src/utils/version.rs
+++ device/larus/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 1, 5, 0],
+    version: [0, 2, 0, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "37e2d4";
+pub const GIT_COMIT_HASH: &str = "69e279";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-04-30T13:49:58+02:0";
+pub const GIT_COMMIT_TIME: &str = "2024-06-12T15:00:55+02:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.1.5";
+pub const GIT_TAG_INFO: &str = "v0.2.0";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [1, 3, 1, 0],
