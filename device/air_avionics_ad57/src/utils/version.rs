03a38e069f310e00329ff16b3c23d3b2763fca0b
Winfried Simon <winfried.simon@gmail.com>
2024-12-19 12:09:45 +0100

    change version

--- device/air_avionics_ad57/src/utils/version.rs
+++ device/air_avionics_ad57/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 2, 1, 0],
+    version: [0, 3, 0, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "695b03";
+pub const GIT_COMIT_HASH: &str = "e76e0e";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2024-07-18T14:35:36+02:0";
+pub const GIT_COMMIT_TIME: &str = "2024-12-19T10:38:40+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.2.1";
+pub const GIT_TAG_INFO: &str = "v0.3.0";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [2, 1, 1, 0],
