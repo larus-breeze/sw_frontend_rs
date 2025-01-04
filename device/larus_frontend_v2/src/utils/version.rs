ce173f25b7d13c7074d50b4e336a1adec49cc0c4
Winfried Simon <winfried.simon@gmail.com>
2025-01-04 13:27:13 +0100

    update changelog to v.0.3.1.3

--- device/larus_frontend_v2/src/utils/version.rs
+++ device/larus_frontend_v2/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 3, 1, 0],
+    version: [0, 3, 1, 3],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "ce819c";
+pub const GIT_COMIT_HASH: &str = "9bdb4d";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2025-01-02T10:11:06+01:0";
+pub const GIT_COMMIT_TIME: &str = "2025-01-04T13:22:40+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.3.1";
+pub const GIT_TAG_INFO: &str = "v0.3.1-3-g9bdb4df";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [1, 3, 2, 0],
