b4bba101a6647a2dc12ce5c22e8bb101d9d56abe
Simsys <winfried.simon@gmail.com>
2025-06-30 13:02:53 +0200

    Sim - delete simulator 'pc'

--- device/pc/copy_assets_air_avionics.sh
+++
@@ -1,5 +0,0 @@
-rm -f src/dev_const.rs || true
-rm -rf assets || true
-
-cp -r ../air_avionics_ad57/assets assets
-cp ../air_avionics_ad57/src/dev_const.rs src/dev_const.rs
--- device/pc/copy_assets_v2.sh
+++
@@ -1,5 +0,0 @@
-rm -f src/dev_const.rs || true
-rm -rf assets || true
-
-cp -r ../larus_frontend_v2/assets assets
-cp ../larus_frontend_v2/src/dev_const.rs src/dev_const.rs
