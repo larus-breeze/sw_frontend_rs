a546604c3c04353d7aacb0772834919b7077047b
Simsys <winfried.simon@gmail.com>
2025-07-08 14:11:31 +0200

    Update version info

--- device/air_avionics_ad57/src/utils/version.rs
+++ device/air_avionics_ad57/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 3, 7, 0],
+    version: [0, 3, 8, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "5739fc";
+pub const GIT_COMIT_HASH: &str = "699844";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2025-05-09T10:15:23+02:0";
+pub const GIT_COMMIT_TIME: &str = "2025-07-08T13:59:34+02:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.3.7";
+pub const GIT_TAG_INFO: &str = "v0.3.8";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [2, 1, 1, 0],
--- device/sim/src/hardware/version.rs
+++ device/sim/src/hardware/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 3, 7, 44],
+    version: [0, 3, 8, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "20634e";
+pub const GIT_COMIT_HASH: &str = "699844";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2025-06-30T09:56:40+02:0";
+pub const GIT_COMMIT_TIME: &str = "2025-07-08T13:59:34+02:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.3.7-44-g20634e5";
+pub const GIT_TAG_INFO: &str = "v0.3.8";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [1, 3, 2, 0],
