cf0169f57f0b6aed332fa191cb576fb9d3eb44a3
Simsys <winfried.simon@gmail.com>
2025-11-09 14:02:17 +0100

    Generate unit pics from svg file

---
+++ assets/create_items.py
@@ -0,0 +1,27 @@
+import os
+
+PICS = [
+    "km_h.png",
+    "mph.png",
+    "kt.png",
+    "m_s.png",
+    "fpm.png",
+    "m.png",
+    "ft.png",
+]
+
+EXPORT_DIRS = [
+    ("assets/size_227x285", "85"),
+    ("assets/size_240x320", "85"),
+    ("assets/size_480x480", "125"),
+]
+
+def export_png(source, f_name, dir_name, dpi):
+    cmd = f"inkscape --export-id={f_name} --export-filename={dir_name}/{f_name} --export-png-color-mode=Gray_1 --export-dpi={dpi} {source}"
+    print(f"writing {dir_name}/{f_name}")
+    os.system(cmd)
+
+
+for f_name in PICS:
+    for dir_name, dpi in EXPORT_DIRS:
+        export_png("assets/items.svg", f_name, dir_name, dpi)
