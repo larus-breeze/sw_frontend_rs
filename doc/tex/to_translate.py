6c9fec94c02326e3152047688e7f5788881e3dba
Simsys <winfried.simon@gmail.com>
2025-12-06 17:07:52 +0100

    Update manual

---
+++ doc/tex/to_translate.py
@@ -0,0 +1,55 @@
+TO_TRANSLATE = (
+    {
+        "src_lang": "DE",
+        "dst_lang": "EN-GB",
+        "file_names": (
+            "ch00-cover-pages.tex",
+            "ch01-general.tex",
+            "ch02-installation.tex",
+            "ch03-operation.tex",
+            "ch04-details.tex",
+            "ch05-configuration.tex",
+        "headers_keep_small": (
+            "and", "of", "in", "the", "on", "from", "to", "for",
+        ),
+        "glossary": {
+            "Bremsklappen": "airbrakes",
+            "Fahrwerk": "landing gear",
+            "Geradeausflugmodus": "straight flight mode",
+            "Haubenblitzer": "canopy flasher",
+            "Kreisflugmodus": "circling mode",
+            "Libelle": "inclinometer",
+            "Polare": "polar",
+            "Sollfahrt": "speed to fly",
+            "Steigen": "climb rate",
+            "Wölbklappen": "flaps",
+        },
+    },
+)
+
+TO_TEST = (
+    {
+        "src_lang": "DE",
+        "dst_lang": "TE",
+        "file_names": (
+            "ch00-cover-pages.tex",
+            "ch01-general.tex",
+            "ch02-installation.tex",
+            "ch03-operation.tex",
+            "ch04-details.tex",
+            "ch05-configuration.tex",
+        ),
+        "glossary": {
+            "Sollfahrt": "speed to fly",
+            "Wölbklappen": "flaps",
+            "Bremsklappen": "airbrakes",
+            "Fahrwerk": "landing gear",
+            "Polare": "polar",
+            "Haubenblitzer": "canopy flasher",
+        },
+        "headers_keep_small": (
+            "and", "of", "in", "the", "on", "from", "to", "for",
+        ),
+    },
+)
+
--- doc/tex/translate.py
+++ doc/tex/translate.py
@@ -1,18 +1,5 @@
-import sys, deepl
-
-TO_TRANSLATE = (
-    {
-        "src_lang": "DE",
-        "dst_lang": "EN-GB",
-        "file_names": (
-            "ch00-cover-pages.tex",
-            "ch01-general.tex",
-            "ch02-installation.tex",
-            "ch03-operation.tex",
-            "ch04-configuration.tex",
-        )
-    },
-)
+import sys, deepl, time
+from to_translate import TO_TRANSLATE, TO_TEST
 
 class Language():
     def __init__(self, lang):
@@ -28,11 +15,9 @@ class Translate():
             api_key = f.read()
         self._deepl = deepl.DeepLClient(api_key)
 
-        self._dictionary = dictionary
-
-
     def check(self):
         for lang in self._languages:
+            lang_key = f"{lang.src_lang}->{lang.dst_lang}"
             for file_name in lang.file_names:
                 in_file = f"{lang.src_lang[:2].lower()}/{file_name}"               
                 print(f"...checking {in_file}")
@@ -40,7 +25,7 @@ class Translate():
                     right = f.read()
                 
                 while True:
-                    left, found, right = self._find_tr(right)
+                    hr, left, found, right = self._find_tr(right)
                     if found is None:
                         break
                     if found.find("\\") >= 0:
@@ -48,8 +33,13 @@ class Translate():
                         sys.exit(1)
 
 
-    def go(self):
+    def go(self, test=False):
         for lang in self._languages:
+            glossary = {}
+            # add ignore tags to glossary
+            for key in lang.glossary.keys():
+                glossary[key] = f"<x>{lang.glossary[key]}</x>"
+
             for file_name in lang.file_names:
                 in_file = f"{lang.src_lang[:2].lower()}/{file_name}"               
                 out_file = f"{lang.dst_lang[:2].lower()}/{file_name}"               
@@ -59,21 +49,73 @@ class Translate():
                 
                 out_str = ""
                 while True:
-                    left, found, right = self._find_tr(right)
+                    hr, left, found, right = self._find_tr(right)
                     if found is None:
+                        # Nothing found, translation of this chunk is finished
                         out_str += left
                         break
                     else:
-                        tr = self._translate(lang, found)
+                        # translate glossary terms
+                        for replace_key in glossary.keys():
+                            pos = found.find(replace_key)
+                            found = found.replace(replace_key, glossary[replace_key])
+                            if pos == 0:
+                                found = found[0].upper() + found[1:]
+                        
+                        # do not translate during testing
+                        if test:
+                            tr = found
+                        else:
+                            # print('translate:', found)
+                            tr = self._translate(lang, found)
+
+                        # after translaten remove xml tags
+                        tr = tr.replace('<x>', '').replace('</x>', '')
+                        
+                        # If header, then respect case sensitivity
+                        if hr:
+                            tr = self._header(lang, tr)
+
                         out_str = out_str + left + tr
 
                 with open(out_file, "w") as f:
                     f.write(out_str)
 
+    def _header(self, lang, found):
+        if lang.headers_keep_small is None:
+            return found
+
+        words = []
+        for word in found.split(" "):
+            if word not in lang.headers_keep_small:
+                if len(word) > 0:
+                    word = word[0].upper() + word[1:]
+
+                sub_words = []
+                for sub_word in word.split('-'):
+                    if sub_word not in lang.headers_keep_small:
+                        if len(sub_word) > 0:
+                            sub_word = sub_word[0].upper() + sub_word[1:]
+                    sub_words.append(sub_word)
+                word = '-'.join(sub_words)
+
+            words.append(word)
+        return " ".join(words)
+
     def _find_tr(self, right):
+        hr = False
         pos = right.find("\\tr{")
-        if pos<0:
-            return (right, None, "")
+        pos2 = right.find("\\hr{")
+        if pos <= 0 and pos2 <=0:
+            return (False, right, None, "")
+
+        if pos < 0:
+            pos = pos2
+            hr = True
+        else:
+            if pos2 >= 0 and pos2 < pos:
+                pos = pos2
+                hr = True
 
         found = ""
         level = 0
@@ -83,7 +125,7 @@ class Translate():
             c = right[pos]
             if c == '}':
                 if level <= 0:
-                    return (right[:pos_begin], found, right[pos:])
+                    return (hr, right[:pos_begin], found, right[pos:])
                 else:
                     level -= 1
             elif c == '{':
@@ -93,17 +135,35 @@ class Translate():
             pos += 1
 
     def _translate(self, lang, in_str):
-        result = self._deepl.translate_text(
-            in_str, 
-            source_lang=lang.src_lang,
-            target_lang=lang.dst_lang,
-            split_sentences="nonewlines",
-            preserve_formatting=True,
-            model_type="prefer_quality_optimized",
-        )
+        while True:
+            try:
+                result = self._deepl.translate_text(
+                    in_str, 
+                    source_lang=lang.src_lang,
+                    target_lang=lang.dst_lang,
+                    split_sentences="nonewlines",
+                    preserve_formatting=True,
+                    model_type="prefer_quality_optimized",
+                    tag_handling="xml",
+                    ignore_tags="x"
+                )
+                break
+            except:
+                sleep_time = 20
+                print(f"*** Error while translating {in_str[:20]}...")
+                print(f"sleep for {sleep_time} seconds...")
+                time.sleep(sleep_time)
+                print("trying again...")
         return result.text
 
 if __name__ == "__main__":
-    translate = Translate(TO_TRANSLATE)
-    translate.check()
-    translate.go()
+    if False:
+        # Test
+        translate = Translate(TO_TEST)
+        translate.check()
+        translate.go(test=True)
+    else:
+        # Translation with deepl
+        translate = Translate(TO_TRANSLATE)
+        translate.check()
+        translate.go()
