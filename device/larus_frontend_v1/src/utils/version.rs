ad8d28b6fb382dcde99540c6e417af0de6db0fc6
Simsys <winfried.simon@gmail.com>
2025-12-12 10:21:00 +0100

    Update version info

--- device/larus_frontend_v1/src/utils/version.rs
+++ device/larus_frontend_v1/src/utils/version.rs
@@ -3,17 +3,17 @@
 use corelib::{HwVersion, SwVersion};
 
 pub const SW_VERSION: SwVersion = SwVersion {
-    version: [0, 3, 8, 0],
+    version: [0, 3, 9, 0],
 };
 
 #[allow(unused)]
-pub const GIT_COMIT_HASH: &str = "699844";
+pub const GIT_COMIT_HASH: &str = "e107b3";
 
 #[allow(unused)]
-pub const GIT_COMMIT_TIME: &str = "2025-07-08T13:59:34+02:0";
+pub const GIT_COMMIT_TIME: &str = "2025-12-12T09:49:34+01:0";
 
 #[allow(unused)]
-pub const GIT_TAG_INFO: &str = "v0.3.8";
+pub const GIT_TAG_INFO: &str = "v0.3.9";
 
 pub const HW_VERSION: HwVersion = HwVersion {
     version: [1, 3, 1, 0],
